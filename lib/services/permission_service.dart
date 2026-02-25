ea546eaf0a6a77b2ddcda4628f5fc5f83c3114ab
rainamaharjan <104052623+rainamaharjan@users.noreply.github.com>
2026-02-25 13:14:08 +0545

    Update permission_service.dart
    permission changed

--- lib/services/permission_service.dart
+++ lib/services/permission_service.dart
@@ -4,19 +4,16 @@ class PermissionService {
   /// Requests necessary permissions and returns true if essential ones are granted.
   Future<bool> checkAndRequestPermissions() async {
     Map<Permission, PermissionStatus> statuses = await [
-      Permission.notification,
-      // Permission.storage,
+      Permission.storage,
       Permission.photos,
       Permission.camera,
     ].request();
 
-    final notificationsGranted =
-        statuses[Permission.notification]?.isGranted == true;
-    // final storageGranted = statuses[Permission.storage]?.isGranted == true;
-    final storageGranted = true;
+    final storageGranted = statuses[Permission.storage]?.isGranted == true;
     final photosGranted = statuses[Permission.photos]?.isGranted == true;
+    final cameraGranted = statuses[Permission.camera]?.isGranted == true;
 
     // Based on your original logic, we need notifications + (storage OR photos)
-    return notificationsGranted && (storageGranted || photosGranted);
+    return cameraGranted && storageGranted && photosGranted;
   }
 }
